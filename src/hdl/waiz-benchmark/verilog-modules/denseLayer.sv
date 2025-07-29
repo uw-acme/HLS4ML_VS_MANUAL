9c5cab2b9d741e72744bd9f188b0faf043ccc6ab
donovanclay <donovan@donovanclay.com>
2025-07-29 14:56:09 +0300

    consolidate verilog modules

---
+++ src/hdl/waiz-benchmark/verilog-modules/denseLayer.sv
@@ -0,0 +1,241 @@
+// Sub Layer Unpacked Version
+//
+// This module represents the dense latency layer of the
+// neural network. In this layer, the input matrix is multiplied
+// with weight matrix. Then the result of the matrix multiplication
+// is added with a matrix of BIASes.
+// NOTE: IDENTICAL to reluDenseLatencyLayer except that different packages
+// are specificed by macro header file
+//
+// Inputs:
+// - clk
+// - reset
+// - input_data
+//
+// Outputs:
+// - output_data
+
+`timescale 1ns / 1ps
+`include "pkg_sel_gru.svh"
+`include "pkg_sel.svh"
+
+// Computes the dot product of the inputs and WEIGHTS then adds that to the BIASes
+module denseLayer #(
+    parameter int WIDTH = 17, // width of fixed point numbers
+    parameter int NFRAC = 10, // number of fractional bits (must be <= WIDTH)
+    parameter int INPUT_SIZE = 32, // number of fixed point numbers going into dense latency layer
+    parameter int OUTPUT_SIZE = 32, // number of fixed point numbers coming out of dense latency layer
+    parameter logic signed [WIDTH-1:0] WEIGHTS [0:INPUT_SIZE-1][0:OUTPUT_SIZE-1] = '{default: '{default: 17'sd0}}, // WEIGHTS for each input to each output
+    parameter logic signed [WIDTH-1:0] BIAS [0:OUTPUT_SIZE-1] = '{default: 17'sd0}, // BIASes for each output
+    parameter int ADDER_TREE_CYCLES = 4 // Number of cycles for adderTree module
+) (
+    input  logic                    clk, 
+    input  logic                    reset,
+    input  logic                    input_ready,
+    output  logic                    output_ready,
+    input  logic signed [WIDTH-1:0] input_data  [0:INPUT_SIZE-1],
+    output logic signed [WIDTH-1:0] output_data [0:OUTPUT_SIZE-1]
+);
+    // check that the right package is being used
+    initial assert($bits(WEIGHTS[0][0]) == WIDTH);
+    
+    
+    logic signed [WIDTH-1:0]   mult         [0:INPUT_SIZE-1][0:OUTPUT_SIZE-1];
+    logic signed [WIDTH*2-1:0] mult_temp    [0:INPUT_SIZE-1][0:OUTPUT_SIZE-1];
+    logic signed [WIDTH-1:0]   accumulator  [0:OUTPUT_SIZE-1];
+    logic signed [WIDTH-1:0]   result       [0:OUTPUT_SIZE-1];
+    logic                      processing;
+    
+    genvar  col, row;
+    // multiplication doubles NFRAC bits in output, we only need NFRAC bits of fractional part,
+    // select top NFRAC bits of the fractional part
+    localparam TOP      = WIDTH+NFRAC-1,
+               BOTTOM   = NFRAC;
+    // Generate a shift add module for each multiplication, shift-add will do optimizations
+    // where it can and leave the rest to Xilinx tools
+    generate 
+        for(row=0; row<INPUT_SIZE; row++) begin : INPUT_SIZE_rows
+            for(col=0; col<OUTPUT_SIZE; col++) begin : OUTPUT_SIZE_cols
+                shift_add #(.WEIGHT ( WEIGHTS[row][col]                 ),
+                            .DEPTH  ( `SA_DEPTH                         ),
+                            .BITS   ( WIDTH                             ),
+                            .NFRAC  ( NFRAC                             )
+                            ) sa (
+                    .clk,
+                    .data_in    ( input_data[row]       ),
+                    .data_out   ( mult_temp[row][col]   )
+                );
+//                assign mult_temp[row][col] = $signed(input_data[row]) * $signed(`SIGMOID_PKG::WEIGHTS[row*OUTPUT_SIZE + col]);
+                assign mult[row][col] = mult_temp[row][col][TOP:BOTTOM];
+           end
+        end
+    endgenerate
+    
+    
+    logic signed [WIDTH-1:0] mult_out [0:INPUT_SIZE-1][0:OUTPUT_SIZE-1];
+    logic signed [WIDTH-1:0] mult_1 [0:INPUT_SIZE-1][0:OUTPUT_SIZE-1];
+    
+    
+    // Determine what configuration DSPs will infer (single cycle or 3-cycle)
+    if (`THREE_CYCLE_MULT) begin
+        always_ff @(posedge clk) begin
+            mult_1 <= mult;
+            mult_out <= mult_1;
+        end
+    end else begin
+        assign mult_out = mult;
+    end
+    
+    // Pipelined adder tree to accumulate the values in mult_out
+    adderTree #(.WIDTH      ( WIDTH         ),
+                .INPUT_SIZE ( INPUT_SIZE    ),
+                .OUTPUT_SIZE( OUTPUT_SIZE   )
+                ) sum_all (
+        .clk,
+        .reset,
+        .input_data ( mult_out      ),
+        .output_data( accumulator   )
+    );
+
+    // Add BIASes to result
+    integer i;
+    always_comb begin
+        // if (input_ready) begin
+        for (i=0; i < OUTPUT_SIZE;i++) begin
+            result[i] = accumulator[i] + BIAS[i];
+        end
+        // end
+    end
+
+    // output
+    typedef enum logic [1:0] {
+        IDLE,
+        PROCESSING,
+        DONE
+    } state_t;
+    
+    state_t state, next_state;
+    int cycle_count;
+    
+    always_ff @(posedge clk or posedge reset) begin
+        if (reset) begin
+            state <= IDLE;
+            cycle_count <= 0;
+        end else begin
+            state <= next_state;
+            if (state == PROCESSING) begin
+                cycle_count <= cycle_count + 1;
+            end else begin
+                cycle_count <= 0;
+            end
+        end
+    end
+    
+    always_comb begin
+        next_state = state;
+        case (state)
+            IDLE: begin
+                if (input_ready) begin
+                    next_state = PROCESSING;
+                end
+            end
+            PROCESSING: begin
+                if (cycle_count >= ADDER_TREE_CYCLES) begin
+                    next_state = DONE;
+                end
+            end
+            DONE: begin
+                next_state = IDLE;
+            end
+        endcase
+    end
+    
+    always_ff @(posedge clk or posedge reset) begin
+        if (reset) begin
+            output_data <= '{default: 0};
+            output_ready <= 0;
+            processing <= 0;
+        end else begin
+            case (state)
+                IDLE: begin
+                    output_ready <= 0;
+                    processing <= 0;
+                end
+                PROCESSING: begin
+                    // Wait for the computation to complete
+                    processing <= 1;
+                end
+                DONE: begin
+                    output_data <= result;
+                    output_ready <= 1;
+                    processing <= 0;
+                end
+            endcase
+        end
+    end
+endmodule
+
+
+
+
+module denseLayer_tb();
+    parameter WIDTH = 8, NFRAC = 0, INPUT_SIZE = 7, OUTPUT_SIZE = 5;
+    logic clk, reset;
+    logic signed [WIDTH-1:0] input_data [0:INPUT_SIZE-1];
+
+    localparam signed [WIDTH-1:0] WEIGHTS [0:INPUT_SIZE-1][0:OUTPUT_SIZE-1] = '{
+        {8'd1, 8'd2, 8'd3, 8'd4, 8'd5},
+        {8'd6, 8'd7, 8'd8, 8'd9, 8'd10},
+        {8'd11, 8'd12, 8'd13, 8'd14, 8'd15},
+        {8'd16, 8'd17, 8'd18, 8'd19, 8'd20},
+        {8'd21, 8'd22, 8'd23, 8'd24, 8'd25},
+        {8'd26, 8'd27, 8'd28, 8'd29, 8'd30},
+        {8'd31, 8'd32, 8'd33, 8'd34, 8'd35}
+    };
+    localparam signed [WIDTH-1:0] BIAS [0:OUTPUT_SIZE-1] = '{
+        {8'd0}, 
+        {8'd0}, 
+        {8'd0}, 
+        {8'd0}, 
+        //{8'd0}, 
+        //{8'd0}, 
+        {8'd0}};
+
+    logic signed [WIDTH-1:0] output_data [0:OUTPUT_SIZE-1];
+    
+    localparam PERIOD = 10;
+    initial begin
+        clk <= 1'b1;
+        forever #(PERIOD/2) clk <= ~clk;
+    end
+    
+    // denseLayer #(
+    //     .WIDTH          ( WIDTH             ),
+    //     .NFRAC          ( NFRAC             ),
+    //     .INPUT_SIZE     ( INPUT_SIZE        ),
+    //     .OUTPUT_SIZE    ( OUTPUT_SIZE       )
+    // ) dut (
+    //     .clk(clk),  .reset(reset),
+    //     .input_data(input_data),
+    //     .output_data(output_data),
+    //     .WEIGHTS(WEIGHTS),
+    //     .BIAS(BIAS)
+    // );
+    
+    initial begin
+        reset = 0;
+        input_data = {{-8'd1},
+                      {8'd2},
+                      {-8'd3},
+                      {8'd4},
+                      {-8'd5},
+                      {8'd6},
+                      {-8'd7}
+                      };
+                      
+        repeat(30) @(posedge clk);
+        
+    end
+    
+    
+endmodule
---
+++ src/hdl/waiz-benchmark/verilog-modules/softmaxLayer.sv
@@ -0,0 +1,215 @@
+`timescale 1ns / 1ps
+
+module softmaxLayer # (
+    parameter N = 10,                // Number of inputs
+    parameter WIDTH = 16,            // Width of input words
+    parameter NFRAC = 10,            // Number of fractional bits
+    parameter MEM_WIDTH = 10,        // Width of the memory lookup indices
+    parameter MEM_NFRAC_EXP = 4,        // Number of fractional bits in the memory lookup indices
+    parameter MEM_NFRAC_INV = 2,        // Number of fractional bits in the memory lookup indices
+    parameter TABLE_WIDTH = 18,      // Width of the table entries
+    parameter TABLE_NFRAC = 10,             // Number of fractional bits
+    parameter EXP_TABLE_PATH = "/home/donovan/code/research/hls4ml/waiz-khan-files/model_7/10_5_prj/myproject_prj/solution1/impl/verilog/softmax_stable_ap_fixed_ap_fixed_10_5_5_3_0_softmax_config16_s_exp_table1_rom.dat",
+    parameter INVERT_TABLE_PATH = "/home/donovan/code/research/hls4ml/waiz-khan-files/model_7/10_5_prj/myproject_prj/solution1/impl/verilog/softmax_stable_ap_fixed_ap_fixed_10_5_5_3_0_softmax_config16_s_invert_table2_rom.dat"
+) (
+    input logic signed [WIDTH-1:0] dataIn [N-1:0],
+    input logic clk,
+    input logic reset,
+    output logic signed [WIDTH-1:0] dataOut [N-1:0]
+);
+
+    // Lookup tables
+    logic signed [TABLE_WIDTH-1:0] exp_table [2**MEM_WIDTH-1:0];
+    logic signed [TABLE_WIDTH-1:0] invert_table [2**MEM_WIDTH-1:0];
+
+    // Intermediate signals
+    wire signed [(2 * TABLE_WIDTH) - 1:0] buffer [N-1:0];
+    logic signed [2*TABLE_WIDTH-1:0] expResult [N-1:0];  // notice that exp table is unsigned, we will need to add a positive sign bit to the result
+    logic signed [2*TABLE_WIDTH-1:0] tempSum;
+    logic signed [2*TABLE_WIDTH-1:0] expSum;
+    logic [MEM_WIDTH-1:0] lookupIndex [N-1:0];
+    logic [MEM_WIDTH-1:0] invertIndex;
+    logic signed [TABLE_WIDTH-1:0] invertVal;
+    // logic signed [MEM_WIDTH-1:0] lookupIndexes [N-1:0];
+    // Initialize tables
+    initial begin
+        // assert that the integer width of word is smaller than that of memory lookup index
+        assert(WIDTH - NFRAC <= MEM_WIDTH - MEM_NFRAC_EXP);
+        // assert that Invert table entry have greater integer width than that of exp table entry
+        // assert(MEM_NFRAC_EXP < MEM_NFRAC_INV);
+        // $readmemh("C:/Users/ljy03/desktop/HLS4ML_VS_MANUAL/src/hdl/RNN/pkg_gen_gru/exp_table_18_10.txt", exp_table, 0, 2**MEM_WIDTH-1);
+        // $readmemh("C:/Users/ljy03/desktop/HLS4ML_VS_MANUAL/src/hdl/RNN/pkg_gen_gru/invert_table_18_10.txt", invert_table, 0, 2**MEM_WIDTH-1);
+        $readmemh(EXP_TABLE_PATH, exp_table, 0, 2**MEM_WIDTH-1);
+        $readmemh(INVERT_TABLE_PATH, invert_table, 0, 2**MEM_WIDTH-1);
+    end
+
+    // Calculate exponentials and sum
+    always_comb begin
+        for (int i = 0; i < N; i++) begin
+            if (MEM_NFRAC_EXP == NFRAC) 
+                lookupIndex[i] = dataIn[i];
+            else if (MEM_NFRAC_EXP < NFRAC)
+                lookupIndex[i] = (dataIn[i] >>> (NFRAC - MEM_NFRAC_EXP));
+            else
+                lookupIndex[i] = (dataIn[i] << (MEM_NFRAC_EXP - NFRAC));
+
+            expResult[i] = {{(TABLE_WIDTH){1'b0}}, exp_table[lookupIndex[i]]};
+        end
+    end
+
+    // Adder Tree
+    adderTree_1D_p4 #(
+        .WIDTH(TABLE_WIDTH * 2),
+        .INPUT_SIZE(N)
+    ) add (
+        .clk(clk),
+        .reset(reset),
+        .input_data(expResult),
+        .output_data(tempSum)
+    );
+
+    // Inverse lookup
+    // TODO: maybe we don't need invert value for negative values.
+    // first cap extreme value (if tempSum >> (TABLE_NFRAC - MEM_NFRAC_INV) is out of range of MEM_WIDTH bit signed value, pick the max / min value)
+
+    assign expSum = tempSum >>> (TABLE_NFRAC - MEM_NFRAC_INV);
+
+
+    always_comb begin
+        if (expSum >= 2**(MEM_WIDTH - 1)) // our table's positve portion only have 512 (2**(MEM_WIDTH-1)) entries
+            invertIndex = {1'b0, {(MEM_WIDTH-1){1'b1}}};
+        else // notice: since expSum could only be positive, we don't need to consider negative values
+        // else if (expSum < -2**(MEM_WIDTH))
+        //     invertIndex = {1'b1, {(MEM_WIDTH-1){1'b0}}};
+        // else
+            invertIndex = expSum[MEM_WIDTH-1:0];
+    end
+
+    assign invertVal = invert_table[invertIndex];
+
+    // Multiply and store the results
+    genvar i;
+    generate
+        for (i = 0; i < N; i++) begin : multiply_and_store
+            mult_op_wrap #(
+                .din_WIDTH(TABLE_WIDTH),
+                .dweight_WIDTH(TABLE_WIDTH),
+                .dout_WIDTH(2 * TABLE_WIDTH)
+            ) mow (
+                .clk(clk),
+                .reset(1'b0),
+                .ce(1'b1),
+                .din(expResult[i][TABLE_WIDTH-1:0]),
+                .dweight(invertVal),
+                .dout(buffer[i])
+            );
+        end
+    endgenerate
+
+    // Output assignment
+    always_ff @(posedge clk) begin
+        for (int i = 0; i < N; i++) begin
+            // // cap max and min values
+            // if (buffer[i][2*TABLE_NFRAC+WIDTH-NFRAC-1 : 2*TABLE_NFRAC - NFRAC] >= 2**(TABLE_WIDTH-1))
+            //     dataOut[i] = {1'b0, {(WIDTH-1){1'b1}}};
+            // else if (buffer[i][2*TABLE_NFRAC+WIDTH-NFRAC-1 : 2*TABLE_NFRAC - NFRAC] < -(2**(TABLE_WIDTH-1)))
+            //     dataOut[i] = {1'b1, {(WIDTH-1){1'b0}}};
+            // else
+
+            // cap disabled
+            dataOut[i] = buffer[i][2*TABLE_NFRAC+WIDTH-NFRAC-1 : 2*TABLE_NFRAC - NFRAC];
+        end
+    end
+endmodule
+
+module softmaxLayer_tb;
+
+    // Parameters
+    localparam N = 3;                // Number of inputs
+    localparam WIDTH = 16;        // Width of input words
+    localparam NFRAC = 10;            // Number of fractional bits
+
+    // Testbench signals
+    logic signed [WIDTH-1:0] dataIn [N-1:0];
+    logic clk;
+    logic signed [WIDTH-1:0] dataOut [N-1:0];
+
+    // Instantiate the softmaxParameterized module
+    softmaxLayer #(
+        .N(N),
+        .WIDTH(WIDTH),
+        .NFRAC(NFRAC)
+    ) uut (
+        .dataIn(dataIn),
+        .clk(clk),
+        .dataOut(dataOut)
+    );
+
+    // Clock generation
+    initial begin
+        clk = 1;
+        forever #5 clk = ~clk;  // 100 MHz clock
+    end
+
+    // Test stimulus
+    initial begin
+        // Initialize inputs
+//        for (int i = 0; i < N; i++) begin
+//            dataIn[i] = $signed(i * 2); // Example data
+//        end
+        
+//        // Wait for a few clock cycles to observe the results
+//        #10;
+
+//        // Apply new data
+//        for (int i = 0; i < N; i++) begin
+//            dataIn[i] = $signed((i + 1) * 3); // Example data
+//        end
+
+//        // Wait for a few clock cycles to observe the results
+//        #10;
+        
+        dataIn[0] = 16'sh0000;
+        dataIn[1] = 16'sh0000;
+        dataIn[2] = 16'sh0000;
+//        dataIn[3] = 16'sh0000;
+        
+        # 200
+
+        dataIn[0] = 16'sh1fff;
+        dataIn[1] = 16'sh0001;
+        dataIn[2] = 16'sh0001;
+//        dataIn[3] = 16'sh0001;
+
+        # 200
+        
+        dataIn[0] = 16'sh1c4a;
+        dataIn[1] = 16'sh117f;
+        dataIn[2] = 16'sha27b;
+//        dataIn[3] = 16'sh0001;
+
+        // dataIn[0] = -16'sh1005;
+        // dataIn[1] = -16'sh0005;
+        // dataIn[2] = -16'sh0005;
+        // dataIn[3] = -16'sh0005;
+
+        # 100
+
+        // dataIn[0] = 16'sh0005;
+        // dataIn[1] = 16'sh0005;
+        // dataIn[2] = 16'sh000a;
+        // dataIn[3] = 16'sh000a;
+
+        // # 100
+
+        // End the simulation
+        $finish;
+    end
+
+    // Monitor the outputs
+    initial begin
+        $monitor("Time = %0t | dataIn = %p | dataOut = %p", $time, dataIn, dataOut);
+    end
+
+endmodule
+
