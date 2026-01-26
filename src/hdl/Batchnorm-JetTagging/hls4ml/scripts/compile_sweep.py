411b20f94d5cfd49fafecfc9110537321118a82a
CalebCeravolo <ceravcal@uw.edu>
2026-01-26 10:06:27 -0800

    Magic

--- src/hdl/Batchnorm-JetTagging/hls4ml/scripts/compile_sweep.py
+++ src/hdl/Batchnorm-JetTagging/hls4ml/scripts/compile_sweep.py
@@ -1,15 +1,16 @@
-#!python
+#! /home/caleb/miniconda3/envs/hls4ml-env/bin/python
 import hls4ml
-import re
 import os
 import numpy as np
-import pandas as pd
+
 from pprint import pprint
-from sys import argv
+from sys import argv, path
 import yaml
+path.insert(1,"/home/caleb/HLS4ML_VS_MANUAL/src/hdl/Batchnorm-JetTagging/")
+from helper_functions import *
 # import subprocess
 from tensorflow.keras.models import load_model # type: ignore
-from sklearn.metrics import accuracy_score
+
 model = load_model('model.h5', compile=False)
 features = ["LUTs", "Registers", "Block RAM Tile", "DSPs", "Bonded IOB"]
 acc = []
@@ -20,37 +21,36 @@ y_test = np.load('y_test.npy')
 test = np.load("X_test.npy")
 #os.environ['PATH'] = r"/tools/Xilinx/2025.1/Vivado/bin:" + os.environ['PATH']
 
-
-# Function that runs entire synthesis, testing, and reporting of 
-# HLS4ML models. Takes in an accuracy in the format "{WIDTH},{NINT}"
-# To modify the model, modify it inside of the function
-def HLS4ML_gen(acc):
+def HLS4ML_gen(acc : tuple[int, int]):
+    """
+    Function that runs entire synthesis, testing, and reporting of 
+    HLS4ML models. Takes in an accuracy in the format (int, )
+    To modify other parameters, modify it inside of the function
+    
+    :param acc: 
+    """
     #try:
-    split = acc.split(",")
-    for i in range(len(split)):
-        split[i]=int(split[i])
-    name = f"hls_final"
-    sweepname = name + f"_{split[0]}_{split[1]}"
+    name = f"hls_rndsat"
+    sweepname = name + f"_{acc[0]}_{acc[1]}"
     if (not os.path.isdir(f'/home/caleb/sweeps/{sweepname}')):
-        model = load_model('model.h5', compile=False)
-        # config = hls4ml.utils.config_from_keras_model(model, granularity='name')
-        f = open("hls_config.yml", "r") #Use yml file for finer tuning
-        config = yaml.safe_load(f)['HLSConfig']
-        f.close()
-        config['Model']['Precision']['default'] = f'ap_fixed<{acc}>'
+        config = hls4ml.utils.config_from_keras_model(model, granularity='model')
+        # f = open("hls_config.yml", "r") #Use yml file for finer tuning
+        # config = yaml.safe_load(f)['HLSConfig']
+        # f.close()
+        config['Model']['Precision']['default'] = f'ap_fixed<{acc[0]},{acc[1]}, AP_RND, AP_SAT>'
         #config['LayerName']['fc1']['Precision']['weight'] = 'ap_fixed<8,2>'
         #config['LayerName']['output']['Precision']['result'] = 'fixed<16,6,RND,SAT>'
         # config['Model']['Strategy'] = 'Latency'
         #config['LayerName']['softmax']['exp_table_t'] = 'ap_fixed<18,8>'
         # config['LayerName']['softmax']['implementation'] = 'stable'
-        # if (int(split[0])>18):
+        # if (int(acc[0])>18):
         # config['LayerName']['softmax']['table_t'] = 'ap_ufixed<18,8>'
         # config['LayerName']['softmax']['reuse_factor'] = 5
-        config['LayerName']['output']['Precision']['result'] = 'ap_fixed<28,10>'
+        # config['LayerName']['output']['Precision']['result'] = 'ap_fixed<28,10>'
         # config['LayerName']['softmax']['Precision']['result'] = 'ap_fixed<18,8>'
-        # config['LayerName']['softmax']['exp_table_t'] = (f'ap_fixed<{acc}>' if (int(split[0])<18) else 'ap_fixed<18,8>')
-        # config['LayerName']['output']['Precision']['result'] = (f'ap_fixed<{acc}>' if (int(split[0])<18) else 'ap_fixed<18,8>')
-        # config['LayerName']['softmax']['Precision']['result'] = (f'ap_fixed<{acc}>' if (int(split[0])<18) else 'ap_fixed<18,8>')
+        # config['LayerName']['softmax']['exp_table_t'] = (f'ap_fixed<{acc}>' if (int(acc[0])<18) else 'ap_fixed<18,8>')
+        # config['LayerName']['output']['Precision']['result'] = (f'ap_fixed<{acc}>' if (int(acc[0])<18) else 'ap_fixed<18,8>')
+        # config['LayerName']['softmax']['Precision']['result'] = (f'ap_fixed<{acc}>' if (int(acc[0])<18) else 'ap_fixed<18,8>')
         hls_model = hls4ml.converters.convert_from_keras_model(model, backend='Vivado', hls_config=config,
                                                                 output_dir=f'/home/caleb/sweeps/{sweepname}',
                                                                 # part='xcu280-fsvh2892-2L-e')
@@ -74,14 +74,14 @@ def HLS4ML_gen(acc):
     timing = extract_time(os.path.join("../reports", f"{sweepname}_timing.rpt"))
 
     #Run accuracy testing
-    accuracy = test_accuracy(sweepname, split)
+    accuracy = test_accuracy(sweepname, acc)
 
     #latency file that the testbench writes new latency values into
     lat_file = f"../results/hls_latency.csv"
     patt = r"[0-9]{1,2}"
 
     #Update testbench with new width values
-    os.system(f'sed -i -E "s/NFRAC = {patt}/NFRAC = {split[0]-split[1]}/g; s/WIDTH = {patt}/WIDTH = {split[0]}/g;" hls_lat_tb.sv')
+    os.system(f'sed -i -E "s/NFRAC = {patt}/NFRAC = {acc[0]-acc[1]}/g; s/WIDTH = {patt}/WIDTH = {acc[0]}/g;" hls_lat_tb.sv')
 
     #Run shell script for getting latency, sending the updated testbench to the proper folder and testing it
     os.system(f"bash get_latency.sh /home/caleb/sweeps/{sweepname}/p_prj/solution1/impl/verilog")
@@ -105,7 +105,7 @@ def HLS4ML_gen(acc):
 
     #Open the csv file and add new data
     with open(csv_name, "a") as f:
-        f.write(f"{split[0]}")
+        f.write(f"{acc[0]}")
         for dat in data:
             f.write(f", {dat}")
         f.write(f", {timing}")
@@ -124,77 +124,6 @@ def HLS4ML_gen(acc):
     #Send report to email
     os.system(f'printf "{Report}" | mail -s "HLS Completion" ceravcal@uw.edu')
 
-# Converts a decimal number to a binary representation
-# Takes in a decimal number and an optional bitwidth argument
-# Returns the binary representation of the number with the given
-# Number of bits. If the number is greater than the bitwidth allows,
-# Returns the maximum possible value for those binary values
-# If number of bits is not given, returns a signed binary number
-# with the minimum possible number of bits to represent it
-def dec_to_bin(number: int | float, bits=-1):
-
-    # Determines if a number is negative
-    neg=False
-    if (number<0):
-        number*=-1
-        number-=1
-        neg=True
-    
-    #rounds number to nearest int
-    number=int(np.round(number, 0))
-    out=""
-    if (bits>0 and number>2**(bits-1)):
-        return "0" + "1"*(bits-1)
-    elif (bits>0 and number<(-1)*2**(bits-1)):
-        return "1" + "0"*(bits-1)
-    
-    while (number>0):
-        res = number%2
-        if (neg):
-            res= 0 if (res==1) else 1
-        out=f"{res}{out}"
-        if (len(out)==(bits-1)):
-            break
-        number=int(number/2)
-        
-    if (neg):
-        out=f"{1}{out}"
-    else: out=f"{0}{out}"
-    if (len(out)==0):
-        out="0"
-    while (len(out)<bits):
-        out=f"{out[0]}{out}"
-    return out
-def extract_data(file, features):
-    "Extracts the feature from a file using Vivado formatting"
-    "| Slice LUTs                 | 66386 |     0 |    433200 | 15.32 |"
-    
-    with open(file) as f:
-        text = f.read()
-    out = []
-    for feature in features:
-        m = re.search(feature + r"\s*\|.*?\|.*?\|.*?\|.*?(\d+\.\d+)", text, re.IGNORECASE)
-        if m:
-            out.append(m.group(1))
-            #return m.group(1)  # third number
-    return out
-    # ind2 = (res[::-1]).find("|")
-    # ind1 = (res[::-1]).find("|", ind2+1)
-    # num = res[-ind1:-ind2-1]
-    # print(num)
-def extract_time(file):
-    with open(file) as f:
-        text = f.read()
-    pat = r"ap_clk\s*(-?\d+\.\d+)" 
-    m = re.search(pat, text, re.IGNORECASE|re.DOTALL)
-    if m:
-        return m.group(1)
-def gen_test(accuracy):
-    test_l = test.flatten()
-    with open("X_test_gen.txt", "w") as f:
-        for num in test_l:
-            num=num*(2**(int(accuracy[0])-int(accuracy[1])))
-            f.write(f"{dec_to_bin(num, accuracy[0])}\n")
 def test_accuracy(model, acc):
     patt = r"[0-9]{1,2}"
     gen_test(acc)
@@ -205,19 +134,18 @@ def test_latency(model, acc):
     patt = r"[0-9]{1,2}"
     os.system(f'sed -i -E "s/NFRAC = {patt}/NFRAC = {acc[0]-acc[1]}/g; s/WIDTH = {patt}/WIDTH = {acc[0]}/g;" hls_lat_tb.sv')
     os.system(f"bash get_latency.sh {model}")
-def keras_test(model):
+def keras_test(model, y_test):
     res =  np.loadtxt(f"{model}/hls_results.csv", delimiter=",")
     res = res[1:]
     acc= accuracy_score((y_test[0:len(res)]).argmax(axis=1), res.argmax(axis=1))
     return acc
-
-    #for i in range(len(scores)):
+#for i in range(len(scores)):
         #print(f"\n{models[i]} accuracy is: {scores[i]} \n")
 #runvars = [2,3,4,5,6,7,8,9,10,12,13]
 # runvars = [12,13]
 # for i in runvars:
 #     #((3*i-2,i))
-#     #print(f"{3*i-2},{i}".split(","))
+#     #print(f"{3*i-2},{i}".acc(","))
 #     HLS4ML_gen(f"{3*i-2},{i}")
 
     #acc_in = f"{3*i-2},6" if i > 6 else f"{3*i-2},{i}"
@@ -226,18 +154,35 @@ def keras_test(model):
     #acc.append(({3*i-2},{i}))
 # HLS4ML_gen("31,11")
 # args = ""
-accs = []
-for i in range(2,6):
-    if not i==11:
+# f = open("hls_config.yml", "r") #Use yml file for finer tuning
+# config = yaml.safe_load(f)['HLSConfig']
+# f.close()
+# config = hls4ml.utils.config_from_keras_model(model, granularity='model')
+# for i in range(2,14):
+#     acc = (3*i-2,i)
+#     config['Model']['Precision']['default'] = f'ap_fixed<{acc[0]},{acc[1]}, AP_RND, AP_SAT>'
+#     hls_model = hls4ml.converters.convert_from_keras_model(model, backend='Vivado', hls_config=config,
+#                                                             output_dir=f'/home/caleb/sweeps/csim',
+#                                                             # part='xcu280-fsvh2892-2L-e')
+#                                                             part='xcvu13p-fhga2104-3-e',
+#                                                             project_name='p')
+#     hls_model.compile()
+
+#     res=hls_model.predict(np.ascontiguousarray(test))
+#     score = accuracy_score((y_test[0:len(res)]).argmax(axis=1), res.argmax(axis=1))
+#     print(acc, score)
+# accs = []
+for i in range(2,10):
+    # if not i==11:
         acc = (3*i-2,i)
         #print((3*i-2,i))
-        # HLS4ML_gen(f"{acc[0]},{acc[1]}")
-        arg = f"/home/caleb/sweeps/hls_please_{acc[0]}_{acc[1]}/p_prj/solution1/impl/verilog"
-        os.system(f"rm {arg}/*.csv")
-        accr = test_accuracy(arg, acc)
-        os.system(f'printf "Acc fin at {acc[0]},{acc[1]} with {accr}"')
+        HLS4ML_gen((acc[0],acc[1]))
+        # arg = f"/home/caleb/sweeps/hls_please_{acc[0]}_{acc[1]}/p_prj/solution1/impl/verilog"
+        # os.system(f"rm {arg}/*.csv")
+        # accr = test_accuracy(arg, acc)
+        # os.system(f'printf "Acc fin at {acc[0]},{acc[1]} with {accr}"')
 # cs = pd.read_csv("../results/util_hls_please.csv", delimiter = ",")
-# cs["Accuracy"] = accs
+# cs["Accuracy"] = accsS
 # cs.to_csv("../results/util_hls_please.csv")
 # print(args)
 # accuracies = []
