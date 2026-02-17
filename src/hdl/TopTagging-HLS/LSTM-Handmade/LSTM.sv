04b7036b50cb3df5062cc82cac89c48b2dd16b82
CalebCeravolo <ceravcal@uw.edu>
2026-02-17 00:38:25 -0800

    Cleanup and work on LSTM

--- src/hdl/TopTagging-HLS/LSTM-Handmade/LSTM.sv
+++ src/hdl/TopTagging-HLS/LSTM-Handmade/LSTM.sv
@@ -1,42 +1,47 @@
 `include "weights_sel.svh"
 import `LSTM_X_WEIGHTS::*;
+import `LSTM_H_WEIGHTS::*;
 
+
+// LSTM Currently designed for only outputting the last state
 module LSTM #( parameter
     WIDTH = 16,
     NINT = 6,
-    INPUT_SIZE1 = 20,
-    INPUT_SIZE2 = 6,
-    OUTPUT_SIZE1 = 20,
-    OUTPUT_SIZE2 = 1,
-    logic signed [WIDTH-1:0] WEIGHTS [INPUT_SIZE-1:0][OUTPUT_SIZE-1:0] = '{default: '{default: 17'sd0}}, // WEIGHTS for each input to each output
-    logic signed [WIDTH-1:0] BIAS [OUTPUT_SIZE-1:0] = '{default: 17'sd0} // BIASes for each output
+    INPUT_SIZE = 6,
+    TIMESTEPS = 20,
+    OUTPUT_SIZE = 20,
 )
 (
     input clk,
     input reset,
-    input logic [WIDTH-1:0] xt [INPUT_SIZE1-1:0][INPUT_SIZE2-1:0],
-    output logic [WIDTH-1:0] ht [INPUT_SIZE-1:0]
+    input input_ready,
+    output output_ready,
+    input logic [WIDTH-1:0] xt [INPUT_SIZE-1:0][TIMESTEPS-1:0],
+    output logic [WIDTH-1:0] ht [OUTPUT_SIZE-1:0]
 );
-
-    logic [WIDTH-1:0] ct_1 [OUTPUT_SIZE1-1:0][OUTPUT_SIZE2-1:0];
-    logic [WIDTH-1:0] ht_1 [OUTPUT_SIZE1-1:0][OUTPUT_SIZE2-1:0];
-    logic [WIDTH-1:0] ct [OUTPUT_SIZE1-1:0][OUTPUT_SIZE2-1:0];
-    logic dense_inputx_ready, dense_outputx_ready;
-    logic [WIDTH-1:0] dense_inputx [INPUT_SIZE1-1:0][INPUT_SIZE2-1:0];
-    logic [WIDTH-1:0] dense_outputx [OUTPUT_SIZE1-1:0][OUTPUT_SIZE2-1:0];
     // x: input
     // h: recurrent information/output
     // c: cell state
-    assign dense_input = xt;
-    
+    logic [WIDTH-1:0] ct_1 [OUTPUT_SIZE-1:0];
+    logic [WIDTH-1:0] ht_1 [OUTPUT_SIZE-1:0];
+    logic [WIDTH-1:0] ct [OUTPUT_SIZE-1:0];
+    logic dense_inputx_ready, dense_outputx_ready;
+    logic [WIDTH-1:0] dense_inputx [INPUT_SIZE-1:0];
+    logic [WIDTH-1:0] dense_outputx [OUTPUT_SIZE-1:0];
+    logic [$clog2(TIMESTEPS)-1:0] curr_step;
+    assign dense_input = xt[curr_step];
+    always_ff @(posedge clk) begin
+        
+    end
+
     
     denseLayer #(
         .WIDTH          ( WIDTH                     ),
         .NFRAC          ( NFRAC                     ),
         .INPUT_SIZE     ( INPUT_SIZE                ),
-        .OUTPUT_SIZE    ( OUTPUT_SIZE               ),
+        .OUTPUT_SIZE    ( OUTPUT_SIZE*4             ),
         .WEIGHTS        ( `LSTM_X_WEIGHTS::weights  ),
-        .BIAS           ( `LSTM_X_WEIGHTS::bias  )
+        .BIAS           ( `LSTM_X_WEIGHTS::bias     )
     ) 
     xt_mult 
     (
@@ -47,9 +52,43 @@ module LSTM #( parameter
         .input_data(dense_inputx),
         .output_data(dense_outputx)
     );
+    logic dense_inputh_ready, dense_outputh_ready;
+    logic [WIDTH-1:0] dense_inputh [OUTPUT_SIZE1-1:0][OUTPUT_SIZE2-1:0];
+    logic [WIDTH-1:0] dense_outputh [OUTPUT_SIZE1-1:0][OUTPUT_SIZE2-1:0];
+    assign dense_input = ht_1;
+    denseLayer #(
+        .WIDTH          ( WIDTH                     ),
+        .NFRAC          ( NFRAC                     ),
+        .INPUT_SIZE     ( OUTPUT_SIZE               ),
+        .OUTPUT_SIZE    ( OUTPUT_SIZE*4             ),
+        .WEIGHTS        ( `LSTM_H_WEIGHTS::weights  ),
+        .BIAS           ( `LSTM_H_WEIGHTS::bias     )
+    ) 
+    ht_mult 
+    (
+        .clk,
+        .reset,
+        .input_ready(dense_inputh_ready),
+        .output_ready(dense_outputh_ready),
+        .input_data(dense_inputh),
+        .output_data(dense_outputh)
+    );
+
+    // Split into 4 parts
+    logic [2*WIDTH-1:0] combined [INPUT_SIZE-1:0];
+    genvar i;
+    generate
+    for (int i=0; i<INPUT_SIZE; i++) begin : combination
+            always_comb 
+                combined[i] = dense_outputh[i]+dense_outputx[i];
+        end
+    endgenerate
+
+    logic [WIDTH-1:0] ft_a, it_a, c_t_a, ot_a;
+    assign {ft_a, it_a, c_t_a, ot_a} = combined;
 
     
-    // ft = sigmoid(Wfh*ht_1+Wfx*xt+bf) basically 2 dense layers, one could have all the bias while the other doesn't (maybe)
+    // ft = sigmoid(Wfh*ht_1+Wfx*xt+bf) 
 
     // it = sigmoid(Wih*ht_1+Wix*xt+bi) 
 
